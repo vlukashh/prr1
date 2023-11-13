cd82d179d096a5df97d5be37b096b1ac6a3cd178
vlukashh <ankalukashenko@gmail.ru>
2023-11-13 22:07:39 +0700

    Настройка отображения списков

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,7 +3,21 @@ from django.contrib import admin
 # Register your models here.
 from .models import Author, Genre, Book, BookInstance
 
-admin.site.register(Book)
-admin.site.register(Author)
+#admin.site.register(Book)
+#admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+#admin.site.register(BookInstance)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+admin.site.register(Author, AuthorAdmin)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+# Register the Admin classes for BookInstance using the decorator
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -29,6 +29,8 @@ class Book(models.Model):
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
 
+
+
     def __str__(self):
         """
         String for representing the Model object.
@@ -41,6 +43,13 @@ class Book(models.Model):
         Returns the url to access a particular book instance.
         """
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+    display_genre.short_description = 'Genre'
 
 import uuid # Required for unique book instances
 
