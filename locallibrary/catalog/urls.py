22978ddcf9dbc5b03ceb10106d431d23f799b2b4
vlukashh <ankalukashenko@gmail.ru>
2023-11-15 17:59:41 +0700

    теперь все готово

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
 ]
 urlpatterns += [
-    path('mybooks/', views.LoanedBooksAllListView.as_view(), name='my-borrowed'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
 
 urlpatterns += [
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -178,3 +178,15 @@ class BookDelete(PermissionRequiredMixin, DeleteView):
             return HttpResponseRedirect(
                 reverse("book-delete", kwargs={"pk": self.object.pk})
             )
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(borrower=self.request.user)
+            .filter(status__exact='o')
+            .order_by('due_back')
+        )
